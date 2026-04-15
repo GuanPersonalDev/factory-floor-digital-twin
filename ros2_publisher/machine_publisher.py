d5e4559862f34b456a95f40f0ea6c8d7c94396fb
Guan <guanpersonalproject@gmail.com>
2026-04-15 22:55:56 +0900

    feat: machine publisher skeleton

--- ros2_publisher/machine_publisher.py
+++ ros2_publisher/machine_publisher.py
@@ -1,3 +1,24 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg
+from std_msgs.msg import String
+import json
+import random
+
+class MachinePublisher(Node):
+    def __init__(self):
+        super().__init__("machine_publisher")
+        # TODO: init 3 publisher
+        pass
+
+    def publishMachineData(self):
+        # TODO: generate machine data and publish
+        pass
+
+def main(args=Node):
+    rclpy.init(args=args)
+    node = MachinePublisher()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
