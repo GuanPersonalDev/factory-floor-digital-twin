e88296a5aa1194fc90353500ae259352de929e10
Guan <guanpersonalproject@gmail.com>
2026-04-18 14:40:47 +0900

    feat: implement ros2 to mqtt bridge

--- bridge/config.py
+++ bridge/config.py
@@ -1,9 +1,9 @@
-MQTT_BROKER_HOST = "localhost"
+MQTT_BROKER_HOST = "10.255.255.254"
 MQTT_BROKER_PORT = 1883
 
 # ROS2 topic -> MQTT topic mapping
 TOPIC_MAP = {
-    "/factory/machine_01/statue": "factory/machine_01/status",
-    "/factory/machine_02/statue": "factory/machine_02/status",
-    "/factory/machine_03/statue": "factory/machine_03/status"
+    "/factory/machine_01/status": "factory/machine_01/status",
+    "/factory/machine_02/status": "factory/machine_02/status",
+    "/factory/machine_03/status": "factory/machine_03/status"
 }
--- bridge/ros2_to_mqtt.py
+++ bridge/ros2_to_mqtt.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-from paho.mqtt.client import mqtt
+import paho.mqtt.client as mqtt
 from config import MQTT_BROKER_HOST, MQTT_BROKER_PORT, TOPIC_MAP
 
 class Ros2MqttBridge(Node):
@@ -9,13 +9,24 @@ class Ros2MqttBridge(Node):
         super().__init__("ros2_mqtt_bridge")
 
         # init MQTT client
-        self.mqttClient_ = mqtt.Client()
+        self.mqttClient_ = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self.mqttClient_.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT)
+        self.mqttClient_.loop_start()
 
+        self.get_logger().info(f"Connected to MQTT Broker : {MQTT_BROKER_HOST}:{MQTT_BROKER_PORT}")
+        for ros2_topic, mqtt_topic in TOPIC_MAP.items():
+            self.create_subscription(
+                String,
+                ros2_topic,
+                lambda msg, t=mqtt_topic: self.onRos2Message(msg, t),
+                10
+            )
         self.get_logger().info("Ros2MqttBridge has already activated")
 
     def onRos2Message(self, msg, mqtt_topic):
         # get message from ROS2 and send to MQTT
-        # TODO: publish msg.data to MQTT
+        self.mqttClient_.publish(mqtt_topic, msg.data)
+        self.get_logger().info(f"Published: {mqtt_topic} -> {msg.data}")
         pass
 
 def main(args=None):
