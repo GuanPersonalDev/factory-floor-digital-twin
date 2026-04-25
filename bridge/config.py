635f17e2cf01ea7326a83dce60f9be9987c3e915
Guan <guanpersonalproject@gmail.com>
2026-04-26 00:41:23 +0900

    import config loader into ros2_to_mqtt

--- bridge/config.py
+++
@@ -1,9 +0,0 @@
-MQTT_BROKER_HOST = "10.255.255.254"
-MQTT_BROKER_PORT = 1883
-
-# ROS2 topic -> MQTT topic mapping
-TOPIC_MAP = {
-    "/factory/machine_01/status": "factory/machine_01/status",
-    "/factory/machine_02/status": "factory/machine_02/status",
-    "/factory/machine_03/status": "factory/machine_03/status"
-}
--- bridge/ros2_to_mqtt.py
+++ bridge/ros2_to_mqtt.py
@@ -1,27 +1,37 @@
+import sys
+from pathlib import Path 
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent)) # add root to Python search path due to I wanna load config.config_loader
+
 import json
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 import paho.mqtt.client as mqtt
 import time
-from config import MQTT_BROKER_HOST, MQTT_BROKER_PORT, TOPIC_MAP
+from config.config_loader import FactoryConfig
+from ros2_to_mqtt_config import MQTT_BROKER_HOST, MQTT_BROKER_PORT
 
 class Ros2MqttBridge(Node):
     def __init__(self):
         super().__init__("ros2_mqtt_bridge")
 
         # init MQTT client
-        self.mqttClient_ = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        self._mqttClient = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         self.connectMqtt()
-        self.mqttClient_.loop_start()
+        self._mqttClient.loop_start()
+        self._config = FactoryConfig()
 
-        for ros2_topic, mqtt_topic in TOPIC_MAP.items():
-            self.create_subscription(
-                String,
-                ros2_topic,
-                lambda msg, t=mqtt_topic: self.onRos2Message(msg, t),
-                10
-            )
+        for param in self._config.parameters:
+            for machine in self._config.machines:
+                ros2_topic = machine.getRosTopic(param)
+                mqtt_topic = machine.getMqttTopic(param)
+                self.create_subscription(
+                    String,
+                    ros2_topic,
+                    lambda msg, t=mqtt_topic: self.onRos2Message(msg, t),
+                    10
+                )
+
         self.get_logger().info("Ros2MqttBridge has already activated")
 
     def connectMqtt(self):
@@ -30,7 +40,7 @@ class Ros2MqttBridge(Node):
         maxRetries = 5
         while retryCount < maxRetries:
             try:
-                self.mqttClient_.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT)
+                self._mqttClient.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT)
                 self.get_logger().info(f"Connected to MQTT Broker : {MQTT_BROKER_HOST}:{MQTT_BROKER_PORT}")
                 return
             except Exception as e:
@@ -44,7 +54,7 @@ class Ros2MqttBridge(Node):
         # get message from ROS2 and send to MQTT
         try:
             data = json.loads(msg.data)
-            self.mqttClient_.publish(mqtt_topic, msg.data)
+            self._mqttClient.publish(mqtt_topic, msg.data)
             self.get_logger().info(f"Published: {mqtt_topic} -> {msg.data}")
         except json.JSONDecodeError as e:
             self.get_logger().warn(f"JSON parse error: {e}")
@@ -54,8 +64,8 @@ class Ros2MqttBridge(Node):
 
     def destroy_node(self):
         # clean up the connection of MQTT when node end
-        self.mqttClient_.loop_stop()
-        self.mqttClient_.disconnect()
+        self._mqttClient.loop_stop()
+        self._mqttClient.disconnect()
         print("Breakout the connection of MQTT")
         super().destroy_node()
 
