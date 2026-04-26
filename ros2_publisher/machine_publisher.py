218ae69828953b44de4805d54e86259e3fe0be0b
Guan <guanpersonalproject@gmail.com>
2026-04-26 11:30:44 +0900

    feat: split topic

--- ros2_publisher/machine_publisher.py
+++ ros2_publisher/machine_publisher.py
@@ -7,51 +7,68 @@ from rclpy.node import Node
 from std_msgs.msg import String
 import json
 import random
-from config.config_loader import FactoryConfig
+from config.config_loader import FactoryConfig, MachineConfig
 
 PUBLISH_INTERVAL = 1.0 # second
 
-class MachinePublisher(Node):
-    def __init__(self):
-        super().__init__("machine_publisher")
-        self._config = FactoryConfig()
+class Topic():
+    def __init__(self, machine :MachineConfig, param, publisher):
+        self._machine_id = machine.machine_id
+        self._param = param
+        self._ros_topic = machine.getRosTopic(param)
+        self._publisher = publisher
+    
+    @property 
+    def machineId(self):
+        return self._machine_id
+    @property
+    def rosTopic(self):
+        return self._ros_topic
 
-        self._machine_publishers_= {}
-        for p in self._config.parameters:
-            for machine in self._config.machines:
-                machine_id = machine.machine_id
-                ros_topic = machine.getRosTopic(p)
-                self._machine_publishers_[machine_id] = self.create_publisher(String, ros_topic, 10)
-
-        self.timer = self.create_timer(PUBLISH_INTERVAL, self.publishMachineData)
-        self.get_logger().info("MachinePublisher has already activated")
-        pass
-
-    def generateMachineData(self, machine_id):
+    def generateMachineData(self, config :FactoryConfig):
         data = {}
-        data["machine_id"] = machine_id
-        for p in self._config.parameters:
-            data[p] = self.generateRandomData(p)
+        data["machine_id"] = self.machineId
+        data[self._param] = self.generateRandomData(self._param, config)
         return data
 
-    def generateRandomData(self, parameter):
+    def generateRandomData(self, parameter, config :FactoryConfig):
         match parameter:
             case "temperature":
                 return round(random.uniform(60.0, 95.0), 1)
             case "vibration":
                 return round(random.uniform(0.1, 5.0), 2)
             case "operation_mode":
-                return random.choice(self._config.operation_mode)
+                return random.choice(config.operation_mode)
         return None
 
+    def publish(self, config :FactoryConfig) -> str:
+        data = self.generateMachineData(config)
+        msg = String()
+        msg.data = json.dumps(data)
+        self._publisher.publish(msg)
+        return msg.data
+
+class MachinePublisher(Node):
+    def __init__(self):
+        super().__init__("machine_publisher")
+        self._config = FactoryConfig()
+
+        self._top_list :list[Topic] = []
+        for p in self._config.parameters:
+            for machine in self._config.machines:
+                ros_topic = machine.getRosTopic(p)
+                publisher = self.create_publisher(String, ros_topic, 10)
+                topic = Topic(machine, p, publisher)
+                self._top_list.append(topic) 
+
+        self.timer = self.create_timer(PUBLISH_INTERVAL, self.publishMachineData)
+        self.get_logger().info("MachinePublisher has already activated")
+        pass
 
     def publishMachineData(self):
-        for machine_id, publisher in self._machine_publishers_.items():
-            data = self.generateMachineData(machine_id)
-            msg = String()
-            msg.data = json.dumps(data)
-            publisher.publish(msg)
-            self.get_logger().info(f"{machine_id} -> {msg.data}")
+        for topic in self._top_list:
+            msg = topic.publish(self._config)
+            self.get_logger().info(f"{topic.rosTopic} -> {msg}")
 
 def main(args=None):
     rclpy.init(args=args)
